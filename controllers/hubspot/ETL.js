38407b587662d3c02cd3c9f1b937e2bfd7fa50d9
virajRevScore <viraj@revscore.io>
2023-05-23 15:48:48 +0530

    branching out to experiment with  bullMQ

--- controllers/hubspot/ETL.js
+++ controllers/hubspot/ETL.js
@@ -10,21 +10,8 @@ const { extractHubspotCRMData } = require("../../services/hubspot/hubspot.dataPi
 
 
 exports.hubspotETLStage1 = async (req , res) => {
-    // const dbClient = await connect()
-    // const db = dbClient.db(req.query.userId)
-    // const collection = db.collection('ThirdPartyAuthDetails')
-    // const authDocs = await collection.find({})
-    // let authList = []
-    // for await  (const doc of authDocs) {
-    //     authList.push(doc)
-    // }
-
-    // const authToken = authList[0]["Hubspot"]['access_token']
-    // const refreshToken = authList[0]["Hubspot"]['refresh_token']
-    // console.log(authToken , refreshToken)
+   
     const result = await extractHubspotCRMData( req.query.userId)
     res.json({ "data_extract_load_length" : result.result.length , "count" : result.result_count})
-//     for (let url in hubspotURLList) {
 
-//     }
 }
